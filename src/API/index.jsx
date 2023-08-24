53854cc447d5ad6df32bc90ef2dbb136e42ab01e
Bill <Bill.1225@live.com>
2023-08-24 15:42:44 -0500

    Post created and API updated

--- src/API/index.jsx
+++ src/API/index.jsx
@@ -1,16 +1,13 @@
 const COHORT_NAME = "2302-acc-ct-web-pt-a"
 const API_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
+// const token = localStorage.getItem("token")
 
 
+export const allPosts = async () => {
 
-export async function summonApi() {
-    try {
-        const response = await fetch(API_URL)
-        const result = await response.jason()
-        console.table(result)
-    } catch (error) {
-        console.error(error)
-    }
+    const response = await fetch(`${API_URL}/posts`);
+    const result = await response.json();
+    console.log(result);
+    return result.data.posts;
+
 }
-
-export default API_URL
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,13 @@ export default function Navbar() {
     <div id="navbar">
       <h1>Stranger's things</h1>
       <Link to="/">
-        <button>Home</button>
+        <h2>Home</h2>
+      </Link>
+      <Link to="/Posts">
+        <h2>Post</h2>
+      </Link>
+      <Link to="/Login">
+        <h2>Login</h2>
       </Link>
     </div>
   );
